62ebcb79f24a39582503d92c4bd67147a03adb74
Himanshu <himanshurobo@gmail.com>
2020-03-23 08:37:21 +0530

    Covid19 web initial Commit

---
+++ app.py
@@ -0,0 +1,233 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from plotly import graph_objs as go
+from dash.dependencies import Input, Output
+import pandas as pd
+import folium 
+
+from getIndiaData import getIndiaData,getCountryWiseData
+
+from apscheduler.scheduler import Scheduler
+import time
+
+
+global dateTime
+global recent_updated
+
+sched = Scheduler() # Scheduler object
+sched.start()
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+baseURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/"
+
+
+
+
+tickFont = {'size':12, 'color':"rgb(30,30,30)", 'family':"Courier New, monospace"}
+
+def loadData(fileName, columnName): 
+    data = pd.read_csv(baseURL + fileName) \
+            .drop(['Lat', 'Long'], axis=1) \
+            .melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name=columnName) \
+            .fillna('<all>')
+    data['date'] = data['date'].astype('datetime64[ns]')
+    return data
+
+
+allData = loadData("time_series_19-covid-Confirmed.csv", "CumConfirmed") \
+    .merge(loadData("time_series_19-covid-Deaths.csv", "CumDeaths")) \
+    .merge(loadData("time_series_19-covid-Recovered.csv", "CumRecovered"))
+
+countries = allData['Country/Region'].unique()
+countries.sort()
+
+
+map_india = folium.Map(location=[20, 80], zoom_start=4.5,tiles='Stamen Toner')
+
+
+def getIndiaStats(map_india):
+
+    df_full_org,dateTime = getIndiaData()
+
+    df_full = df_full_org.drop(df_full_org.tail(1).index,inplace=False)
+
+    for lat, lon, value, name, case_indian, case_foreign, cured, death in zip(df_full['Latitude'], df_full['Longitude'], df_full['Active Cases'], df_full['Name of State / UT'], df_full['Total Confirmed cases (Indian National)'],df_full['Total Confirmed cases ( Foreign National )'],df_full['Cured/Discharged/Migrated'],df_full['Death']):
+        folium.CircleMarker([lat, lon],
+                            radius=value*0.7,
+                            tooltip = ('<strong>State</strong>: ' + str(name).capitalize() + '<br>'
+                                    '<strong>Active Cases</strong>: ' + str(value) + '<br>'
+                                    '<strong>Indian Cases</strong>: ' + str(case_indian) + '<br>'
+                                    '<strong>Foreign Cases</strong>: ' + str(case_foreign) + '<br>'
+                                    '<strong>Cured ases</strong>: ' + str(cured) + '<br>'
+                                    '<strong>Death</strong>: ' + str(death) + '<br>'),
+                            color='red',
+                            
+                            fill_color='red',
+                            fill_opacity=0.3 ).add_to(map_india)
+    return map_india,dateTime
+
+
+map_india,dateTime = getIndiaStats(map_india)    
+map_india.save('india_data.html')
+
+
+
+map_world = folium.Map(location=[20, 30], zoom_start=2.5,tiles='Stamen Toner')
+
+def getCountryWiseDataStats(map_world):
+
+    df_full,recent_updated = getCountryWiseData()
+
+    # df_full = df_full_org.drop(df_full_org.tail(1).index,inplace=False)
+
+    for lat, lon, value, name, last_updated,confirmed,deaths,recovered in zip(df_full['Latitude'], df_full['Longitude'], df_full['Active Cases'], df_full['Country_Region'],df_full['Last_Update'],df_full['Confirmed'],df_full['Deaths'],df_full['Recovered']):
+        folium.CircleMarker([lat, lon],
+                            radius=value*0.005,
+                            tooltip = ('<strong>State</strong>: ' + str(name).capitalize() + '<br>'
+                                    '<strong>Active Cases</strong>: ' + str(value) + '<br>'
+                                    '<strong>Last_updated</strong>: ' + str(last_updated) + '<br>'
+                                    '<strong>Confirmeds</strong>: ' + str(confirmed) + '<br>'
+                                    '<strong>Deaths </strong>: ' + str(deaths) + '<br>'
+                                    '<strong>Recovered </strong>: ' + str(recovered) + '<br>'),
+                            color='red',
+                            
+                            fill_color='red',
+                            fill_opacity=0.3 ).add_to(map_world)
+    return map_world,recent_updated
+
+map_world,recent_updated = getCountryWiseDataStats(map_world)    
+map_world.save('world_data.html')
+
+
+
+
+app.layout = html.Div(
+    style={ 'font-family':"Courier New, monospace" },
+    children=[
+    html.H1('Case History of the Coronavirus (COVID-19)'),
+    html.Div(className="row", children=[
+        html.Div(className="four columns", children=[
+            html.H5('Country'),
+            dcc.Dropdown(
+                id='country',
+                options=[{'label':c, 'value':c} for c in countries],
+                value='India'
+            )
+        ]),
+        html.Div(className="four columns", children=[
+            html.H5('State / Province'),
+            dcc.Dropdown(
+                id='state'
+            )
+        ]),
+        html.Div(className="four columns", children=[
+            html.H5('Selected Metrics'),
+            dcc.Checklist(
+                id='metrics',
+                options=[{'label':m, 'value':m} for m in ['Confirmed', 'Deaths', 'Recovered']],
+                value=['Confirmed', 'Deaths','Recovered']
+            )
+        ])
+    ]),
+    dcc.Graph(
+        id="plot_new_metrics",
+        config={ 'displayModeBar': False }
+    ),
+    dcc.Graph(
+        id="plot_cum_metrics",
+        config={ 'displayModeBar': False }
+    ),
+
+html.H5('India Map View Last UpdatedAt :'+dateTime),
+html.Iframe(id = 'map_india',  srcDoc = open("india_data.html",'r').read(), width='100%',height='600'),
+
+html.H5('World Map View Last UpdatedAt : '+recent_updated),
+html.Iframe(id = 'map_world',  srcDoc = open("world_data.html",'r').read(), width='100%',height='600')
+
+
+])
+
+@app.callback(
+    [Output('state', 'options'), Output('state', 'value')],
+    [Input('country', 'value')]
+)
+def update_states(country):
+    states = list(allData.loc[allData['Country/Region'] == country]['Province/State'].unique())
+    states.insert(0, '<all>')
+    states.sort()
+    state_options = [{'label':s, 'value':s} for s in states]
+    state_value = state_options[0]['value']
+    return state_options, state_value
+
+def nonreactive_data(country, state):
+    data = allData.loc[allData['Country/Region'] == country]
+    if state == '<all>':
+        data = data.drop('Province/State', axis=1).groupby("date").sum().reset_index()
+    else:
+        data = data.loc[data['Province/State'] == state]
+    newCases = data.select_dtypes(include='int64').diff().fillna(0)
+    newCases.columns = [column.replace('Cum', 'New') for column in newCases.columns]
+    data = data.join(newCases)
+    data['dateStr'] = data['date'].dt.strftime('%b %d, %Y')
+    return data
+
+def barchart(data, metrics, prefix="", yaxisTitle=""):
+    figure = go.Figure(data=[
+        go.Bar( 
+            name=metric, x=data.date, y=data[prefix + metric],
+            marker_line_color='rgb(0,0,0)', marker_line_width=1,
+            marker_color={ 'Deaths':'rgb(200,30,30)', 'Recovered':'rgb(30,200,30)', 'Confirmed':'rgb(100,140,240)'}[metric]
+        ) for metric in metrics
+    ])
+    figure.update_layout( 
+            barmode='group', legend=dict(x=.05, y=0.95, font={'size':15}, bgcolor='rgba(240,240,240,0.5)'), 
+            plot_bgcolor='#FFFFFF', font=tickFont) \
+        .update_xaxes( 
+            title="", tickangle=-90, type='category', showgrid=True, gridcolor='#DDDDDD', 
+            tickfont=tickFont, ticktext=data.dateStr, tickvals=data.date) \
+        .update_yaxes(
+            title=yaxisTitle, showgrid=True, gridcolor='#DDDDDD')
+    return figure
+
+@app.callback(
+    Output('plot_new_metrics', 'figure'), 
+    [Input('country', 'value'), Input('state', 'value'), Input('metrics', 'value')]
+)
+def update_plot_new_metrics(country, state, metrics):
+    data = nonreactive_data(country, state)
+    return barchart(data, metrics, prefix="New", yaxisTitle="New Cases per Day")
+
+@app.callback(
+    Output('plot_cum_metrics', 'figure'), 
+    [Input('country', 'value'), Input('state', 'value'), Input('metrics', 'value')]
+)
+def update_plot_cum_metrics(country, state, metrics):
+    data = nonreactive_data(country, state)
+    return barchart(data, metrics, prefix="Cum", yaxisTitle="Cumulated Cases")
+
+
+def job():
+    print("I'm working...")
+    map_india1,dateTime = getIndiaStats(map_india)    
+    map_india1.save('india_data.html')
+
+    map_world1,recent_updated = getCountryWiseDataStats(map_world)    
+    map_world1.save('world_data.html')
+
+    allData = loadData("time_series_19-covid-Confirmed.csv", "CumConfirmed") \
+    .merge(loadData("time_series_19-covid-Deaths.csv", "CumDeaths")) \
+    .merge(loadData("time_series_19-covid-Recovered.csv", "CumRecovered"))
+
+    countries = allData['Country/Region'].unique()
+    countries.sort()
+
+sched.add_interval_job(job,minutes=15)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ getIndiaData.py
@@ -0,0 +1,203 @@
+import requests
+import lxml.html as lh
+import pandas as pd
+
+
+from fuzzywuzzy import fuzz 
+from fuzzywuzzy import process
+
+def checker(wrong_options,correct_options):
+    response = []    
+    for wrong_option in wrong_options:
+        x=process.extractOne(wrong_option,correct_options)
+        response.append((x[0],x[1],wrong_option))
+    return response
+
+def getIndiaData():
+    # url='https://www.latlong.net/search.php?keyword=bihar'
+    url='https://www.mohfw.gov.in/'
+    #Create a handle, page, to handle the contents of the website
+    page = requests.get(url)
+    #Store the contents of the website under doc
+    doc = lh.fromstring(page.content)
+    #Parse data that are stored between <tr>..</tr> of HTML
+    tr_elements = doc.xpath('//tr')
+    
+
+    i=0
+    p_elements = doc.xpath('//p')
+
+    print(len(p_elements))
+    last = int(len(p_elements)-1)
+    #For each row, store each first element (header) and an empty list
+    for t in p_elements:
+        i+=1
+        name=t.text_content()
+        print ('%d:"%s"'%(i,name))
+        # col.append((name,[]))
+
+    dateTime = (p_elements[last]).text_content()
+    
+    dateTime = dateTime[37:-1]
+    #print(tr_elements)
+
+    #Check the length of the first 12 rows
+    # [len(T) for T in tr_elements[:12]]
+
+
+
+    tr_elements = doc.xpath('//tr')
+    #Create empty list
+    col=[]
+    i=0
+    #For each row, store each first element (header) and an empty list
+    for t in tr_elements:
+        i+=1
+        name=t.text_content()
+        # #print ('%d:"%s"'%(i,name))
+        col.append((name,[]))
+
+
+    #Since out first row is the header, data is stored on the second row onwards
+    for j in range(1,len(tr_elements)):
+        #T is our j'th row
+        T=tr_elements[j]
+        
+        #If row is not of size 10, the //tr data is not from our table 
+        if len(T)!=10:
+            break
+        
+        #i is the index of our column
+        i=0
+        
+        #Iterate through each element of the row
+        for t in T.iterchildren():
+            data=t.text_content() 
+            #Check if row is empty
+            if i>0:
+            #Convert any numerical value to integers
+                try:
+                    data=int(data)
+                except:
+                    pass
+            #Append the data to the empty list of the i'th column
+            col[i][1].append(data)
+            #Increment i for the next column
+            i+=1
+
+
+    [len(C) for (title,C) in col]
+
+
+
+
+    data = []
+
+    for (title,C) in col[:-1]:
+        #print("--->>>",title.split("\n"))
+        hello = [x.strip() for x in title.split("\n") if x.strip() != '']
+        # data.append(title.split("\n"))
+        # #print(hello)
+        data.append(hello)
+
+
+
+    df = pd.DataFrame(data)
+    # #print(df.head())    
+        
+    # df.set_index('S. No.', inplace=True)
+    # Dict={title:column for (title,column) in col}
+    # df=pd.DataFrame(Dict)
+    # #print(Dict)
+
+
+    index = df.loc[(df[0] == 'S. No.')].index[0]
+
+
+    df = df.iloc[index:]
+
+    df.reset_index(drop=True,inplace=True)
+
+    df.columns = df.iloc[0]
+    df.drop(df.index[0],inplace=True)
+    # using dictionary to convert specific columns 
+    convert_dict = {'Total Confirmed cases (Indian National)': int, 
+                    'Total Confirmed cases ( Foreign National )': int,
+                    'Cured/Discharged/Migrated' : int,
+                    'Death' : int
+                } 
+    df = df.astype(convert_dict)
+    # #print(df.info())
+    df['Active Cases'] = df['Total Confirmed cases (Indian National)'] + df['Total Confirmed cases ( Foreign National )'] \
+        - df['Cured/Discharged/Migrated'] - df['Death']
+
+
+
+    #print(df.tail())
+    indiaStateLatLong = pd.read_csv('./indiaStateLatLong.csv',index_col=0)
+
+    # # creating and passsing series to new column 
+
+    # #print(indiaStateLatLong.head())
+
+    str2Match = df['Name of State / UT'].fillna('######').tolist()
+    strOptions =indiaStateLatLong.index.fillna('######').tolist()
+
+
+
+    # #print(str2Match)
+
+    name_match=checker(str2Match,strOptions)
+
+    # #print(name_match)
+
+    match_df = pd.DataFrame(list(name_match), columns=['States','score','Name of State / UT'])
+    # #print(match_df)
+
+    merged_df = match_df.merge(df, how = 'inner', on = ['Name of State / UT'])
+    merged_df = merged_df.merge(indiaStateLatLong, how = 'inner', on = ['States'])
+
+    #print(merged_df.head())
+
+    print("Get Indian Data")
+
+    convertList = list(convert_dict.keys())
+    convertList.append('Active Cases')
+    merged_df.drop(columns=['States','score'],inplace=True)
+    # #print(indiaStateLatLong.loc[name_match[0]])
+    merged_df.set_index('S. No.',inplace=True)
+    merged_df.loc['Total'] = merged_df[convertList].sum(axis=0,numeric_only=True)
+    merged_df.to_csv('./text.csv',index=False)
+    return merged_df,dateTime
+
+
+
+
+def getCountryWiseData():
+    print("Get Country Wise Data")
+    dataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv'
+
+    df = pd.read_csv(dataUrl)
+    
+
+    mean_active = df.Active.dropna().mean()
+    max_active = df.Active.dropna().max()
+    min_active = df.Active.dropna().min()
+
+    df['mean_active'] = df['Active'].apply(lambda x: (x - mean_active ) / (max_active -min_active ))
+
+    df.rename(columns={"Lat": "Latitude", "Long_": "Longitude", "Active" : "Active Cases"},inplace=True)
+
+    #print(df.head())
+
+    # df = df[:5]
+    df1 = df.copy()
+    df['StartDate'] = pd.to_datetime(df['Last_Update'])
+    recent_date = df['StartDate'].max()
+
+    df['Active Cases'] = df['Confirmed'] + df['Deaths'] \
+        - df['Recovered']
+    df.to_csv('./text1.csv',index=False)
+    return df,str(recent_date)
+
+
